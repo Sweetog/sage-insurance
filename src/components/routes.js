23b61d15f9ea9b8a011f519f91024800f08736e1
e-ian <ianemma70@gmail.com>
2019-12-20 19:27:19 +0300

    (project-setup): setup project with create-react-app
    - set up redux
    - set up react-router
    - set up sass processing on client
    - set up project folder structure

---
+++ src/components/routes.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./home";
+
+class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Routes;
